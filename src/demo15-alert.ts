14bf22f60a61d5682e1a525d6897c1d409b475ff
balaji-githubstore <balajidinakaran1@gmail.com>
2026-05-10 16:01:56 +0530

    Alert

---
+++ src/demo15-alert.ts
@@ -0,0 +1,19 @@
+import { chromium, Page } from "playwright"
+
+const browser = await chromium.launch({ headless: false, channel: "chrome" });
+const context = await browser.newContext();
+const page = await context.newPage();
+await page.goto("https://www.nasscom.in/nasscom-membership");
+
+//register the event upfront to handle the alert. 
+await page.on("dialog", async dialog => {
+    console.log(dialog.message())
+    await dialog.accept();
+})
+
+
+await page.locator("xpath=//a[text()='Calculate Fee']").click();
+
+
+await page.waitForTimeout(5000);
+await browser.close();
